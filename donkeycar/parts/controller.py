de9f5cc4a389ee57c38459c0b9b37e5296710d5b
Francis Duvivier <francis.duvivier@eurofins-digitaltesting.com>
2023-04-29 00:13:59 +0200

    Add emergency stop for 2.4Ghz Controller

--- donkeycar/parts/controller.py
+++ donkeycar/parts/controller.py
@@ -262,6 +262,8 @@ class RCReceiver:
         self.RECORD = cfg.AUTO_RECORD_ON_THROTTLE
         self.debug = debug
         self.mode = 'user'
+        self.lastSideButtonState = None
+        self.emergency_stopped = False
         self.is_action = False
         self.invert = cfg.PIGPIO_INVERT
         self.jitter = cfg.PIGPIO_JITTER
@@ -293,6 +295,23 @@ class RCReceiver:
                     if channel.high_tick is not None:                    
                         channel.tick = pigpio.tickDiff(channel.high_tick, tick)
 
+    def toggle_mode(self):
+        '''
+        switch modes from:
+        user: human controlled steer and throttle
+        local_angle: ai steering, human throttle
+        local: ai steering, ai throttle
+        '''
+        if (self.emergency_stopped):
+          self.emergency_stopped = False
+        elif self.mode == 'user':
+          self.mode = 'local'
+        elif self.mode == 'local':
+          self.emergency_stopped = True
+        else:
+          self.mode = 'user'
+        print('new mode:', self.mode)
+
     def pulse_width(self, high):
         """
         :return: the PWM pulse width in microseconds.
@@ -318,6 +337,8 @@ class RCReceiver:
             else:
                 self.signals[i] += self.MIN_OUT
             i += 1
+        self.signals[0] *= self.steering_scale
+        self.signals[1] *= self.throttle_scale
         if self.debug:
             logger.info(f'RC CH1 signal:{round(self.signals[0], 3)}, RC CH2 signal:{round(self.signals[1], 3)}, RC CH3 signal:{round(self.signals[2], 3)}')
 
@@ -335,6 +356,16 @@ class RCReceiver:
             # pass through default value
             is_action = recording if recording is not None else False
         return self.signals[0], self.signals[1], self.mode, is_action
+
+    def emergency_stop(self):
+        '''
+        initiate a series of steps to try to stop the vehicle as quickly as possible
+        '''
+        print('In Emergency stop mode, press sidebutton!!! ' + str(time.time()))
+        self.mode = "user"
+        self.signals[0] = 0
+        self.signals[1] = -0.3
+        return self.signals[0], self.signals[1], self.mode, False
 
     def shutdown(self):
         """
